336743e6b17890a2016025e514d217f7b1293fe7
VangieCB <ecomes@hotmail.com>
2023-05-29 00:05:18 -0300

    finC6

--- C6/src/components/AccountCard.jsx
+++ C6/src/components/AccountCard.jsx
@@ -1,26 +1,19 @@
 import accounts from './accounts.json'
 
-/*Muestra la lista de links que muestren el nombre del usuario 
-y redirijan a https://twitter.com/${account}*/
-
 export function AccountCard() {
 
-    return (
+    return(
         <>
-          <section>
-                <h1>Usuarios</h1>
-                {accounts.map(account =>(
+        <section>
+            <h1>Usuarios</h1>
+            {accounts.map(account =>(
+                <div className='card' key={account.account}>
+                    <h2>{account.account}</h2>
+                    <a href={`https://twitter.com/${account.userName}`}>Perfil Usuario</a>
+                </div>
 
-                    <div className='card' key={account.account}>
-                        <h2>{account.account}</h2>
-                        <a href='https://twitter.com/${account.username}'></a>
-                    </div>
-                )
-                    )}
-       
-            </section>
+            ))}
+        </section>
         </>
-
-
     )
 }
--- C6/src/components/UserCard.jsx
+++ C6/src/components/UserCard.jsx
@@ -3,11 +3,28 @@ import users from './users.json'
 /*Muestra la lista de usuarios con su apodo, y la lista de sus mascotas 
 con el formato ${nombre} (${edad}): ${color}*/
 
-export  function UserCard() {
+export function UserCard() {
 
     return (
-     <></>
+        <>
+            <section>
+                <h1>🐾Mascotas de Usuarios🐾</h1>
+                {users.map(user => (
+                    <div className='card' key={user.email}>
+                        <h2>Apodo del Usuario: {user.apodo}</h2>
+                            <ul>
+                                {user.mascotas.map(mascota => (
+                                    <li key={mascota.nombre}>
+                                        Nombre: {mascota.nombre} - Edad: {mascota.edad} - Color {mascota.color}
+                                    </li>
+                                ))}
+                            </ul>
+                    </div>
+                ))}
+            </section>
 
-        
+        </>
+
+
     )
 }
